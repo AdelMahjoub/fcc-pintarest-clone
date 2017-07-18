392210d554de0eb59b09bea33d667f7883bac4d0
Adel Mahjoub <dragonhunter250@gmail.com>
2017-07-18 19:52:47 +0200

    handle images errors

--- public/js/main.js
+++ public/js/main.js
@@ -1,18 +1,9 @@
-function renderGrid() {
-  
-  let fallbackSrc = 'http://via.placeholder.com/256x256';
-  let images = document.querySelectorAll('.image img');
-  const wall = document.getElementById('wall');
-  const blocks = document.getElementById('wall') ? document.getElementById('wall').children : [];
-  const SPACE = 20;
-  const COLUMNS = 5;
-  let x = y = 0;
+function buildWall(blocks, COLUMNS, SPACE) {
 
   for(let i = 1; i < blocks.length; i++) {
     if(i % COLUMNS === 0) {
       y = (blocks[i - COLUMNS].offsetTop + blocks[i - COLUMNS].offsetHeight) + SPACE;
       blocks[i].style.top = `${y}px`;
-      
     } else {
       if(blocks[i - COLUMNS]) {
         y = (blocks[i - COLUMNS].offsetTop + blocks[i - COLUMNS].offsetHeight) + SPACE;
@@ -22,16 +13,27 @@ function renderGrid() {
       blocks[i].style.left = `${x}px`;
     }
   }
+}
 
-  if(images) {
-    images.forEach(image => {
-      image.addEventListener('error', f = function(e) {
-        image.setAttribute('src', fallbackSrc);
-        image.removeEventListener('error', f);
-        renderGrid();
-      },false);
-    });
-  }
+function renderGrid() {
+  
+  let fallbackSrc = 'http://via.placeholder.com/256x256';
+  let images = document.querySelectorAll('.image img');
+  const wall = document.getElementById('wall');
+  const blocks = document.getElementById('wall') ? document.getElementById('wall').children : [];
+  const SPACE = 20;
+  const COLUMNS = 5;
+
+  buildWall(blocks, COLUMNS, SPACE);
+  
+  images.forEach(image => {
+    image.addEventListener('load', function(e) {
+     buildWall(blocks, COLUMNS, SPACE);
+    }, false);
+    image.addEventListener('error', function(e) {
+      image.setAttribute('src', fallbackSrc);
+    },false);
+  });
 
 }
 window.addEventListener('load', renderGrid, false);
